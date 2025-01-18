fccbddedddffcfaf7864eb076201cb7445883d27
Alex P <a117a117a@gmail.com>
2025-01-18 20:58:34 +0100

    get population by country

---
+++ Week4/homework/ex1-aggregation/get_population_by_country.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+async function getTotalPopulationByCountry(country) {
+    const uri = process.env.MONGODB_URL || "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        const db = client.db("population_data");
+        const collection = db.collection("population");
+
+        const result = await collection.aggregate([
+            { $match: { Country: country } },
+            { $group: { _id: "$Year", countPopulation: { $sum: { $add: ["$M", "$F"] } } } },
+            { $sort: { _id: 1 } }
+        ]).toArray();
+
+        console.log(`Total population by year for ${country}:`);
+        console.table(result);
+        return result;
+    } catch (err) {
+        console.error("Error:", err);
+    } finally {
+        await client.close();
+    }
+}
+
+getTotalPopulationByCountry("Netherlands");
