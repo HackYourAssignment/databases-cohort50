c61e99f005d73bb3206296cb2c48e920c4eaed3e
Vlada B <vlada.bessikalo@gmail.com>
2025-01-22 18:11:02 +0100

    complete ex1

--- Week4/homework/ex1-aggregation/index.js
+++ Week4/homework/ex1-aggregation/index.js
@@ -5,14 +5,11 @@ import csv from 'csv-parser';
 
 const client = new MongoClient(process.env.MONGODB_URL);
 
+const db = client.db('databaseWeek4');
+const collection = db.collection('population_pyramid_1950-2022');
+
 async function importCSVtoMongoDB() {
     try {
-        await client.connect();
-        console.log('Connected to MongoDB');
-
-        const db = client.db('databaseWeek4');
-        const collection = db.collection('population_pyramid_1950-2022');
-
         const records = [];
 
         fs.createReadStream('population_pyramid_1950-2022.csv')
@@ -33,12 +30,83 @@ async function importCSVtoMongoDB() {
                     const result = await collection.insertMany(records);
                     console.log(`${result.insertedCount} records inserted.`);
                 }
-                client.close();
             });
     } catch (err) {
         console.error('Error:', err);
-        client.close();
     }
 }
 
-importCSVtoMongoDB();
+async function totalPopulationByCountry(country) {
+    try {
+        const result = await collection
+            .aggregate([
+                { $match: { Country: country } },
+                {
+                    $group: {
+                        _id: '$Year',
+                        countPopulation: { $sum: { $add: ['$M', '$F'] } }
+                    }
+                },
+                { $sort: { _id: 1 } }
+            ])
+            .toArray();
+
+        console.log(result);
+
+        return result;
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+async function continentDataByYearAndAge(year, age) {
+    try {
+        const continents = [
+            'AFRICA',
+            'ASIA',
+            'EUROPE',
+            'LATIN AMERICA AND THE CARIBBEAN',
+            'NORTHERN AMERICA',
+            'OCEANIA'
+        ];
+        const result = await collection
+            .aggregate([
+                {
+                    $match: {
+                        Year: year,
+                        Age: age,
+                        Country: { $in: continents }
+                    }
+                },
+                {
+                    $addFields: {
+                        TotalPopulation: { $add: ['$M', '$F'] }
+                    }
+                }
+            ])
+            .toArray();
+
+        console.log(result);
+
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function main() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        await importCSVtoMongoDB();
+        await totalPopulationByCountry('Netherlands');
+        await continentDataByYearAndAge(2020, '100+');
+    } catch (err) {
+        console.error('Error:', err);
+    } finally {
+        await client.close();
+        console.log('Disconnected!');
+    }
+}
+
+main();
